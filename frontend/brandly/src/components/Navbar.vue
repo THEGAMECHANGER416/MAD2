e3ff389d41b34aa11d1085424089302a6be4d6bb
Archit Kohli <architkohli321@gmail.com>
2024-05-24 17:55:37 +0530

    Signup Added

--- frontend/brandly/src/components/Navbar.vue
+++ frontend/brandly/src/components/Navbar.vue
@@ -1,21 +1,57 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-        <div class="container-fluid d-flex justify-content-between align-items-center">
-            <a class="navbar-brand d-flex align-items-center" href="#">
-                <img src="../assets/img/logo.svg" alt="Logo" width="32" height="30" class="align-text-top">
-                <h3 class="title mb-0 ml-2">Brandly</h3>
-            </a>
-            <div class="d-flex align-items-center">
-                <router-link to="/admin" class="link-text mx-2">Admin</router-link>
-            </div>
-        </div>
-    </nav>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary fixed-top">
+        <!-- Container wrapper -->
+        <div class="container-fluid">
+            <!-- Toggle button -->
+            <button data-mdb-collapse-init class="navbar-toggler" type="button"
+                data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
 
+            <!-- Collapsible wrapper -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Navbar brand -->
+                <a class="navbar-brand mt-2 mt-lg-0" href="#">
+                    <img src="../assets/img/logo.svg" height="30"
+                        alt="Brandly Logo" loading="lazy" />
+                    <p class="mb-0 title">Brandly</p>
+                </a>
+            </div>
+            <!-- Collapsible wrapper -->
+
+            <!-- Right elements -->
+            <div class="d-flex align-items-center">
+                <!-- Right links -->
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link text-black" href="#">Admin</a>
+                    </li>
+                </ul>
+                <!-- Right links -->
+            </div>
+            <!-- Right elements -->
+        </div>
+        <!-- Container wrapper -->
+    </nav>
+    <!-- Navbar -->
 </template>
 <script>
+// Initialization for ES Users
+import { Dropdown, Collapse, initMDB } from "mdb-ui-kit";
 
 export default {
-    name: 'NavBar'
+    name: 'NavBar',
+    mounted() {
+        // Ensure that MDB is initialized after the component is mounted
+        this.initMDBInputs();
+    },
+    methods: {
+        initMDBInputs() {
+            initMDB({ Dropdown, Collapse });
+        }
+    }
 }
 </script>
 <style scoped>
@@ -26,7 +62,7 @@ export default {
 }
 
 .link-text {
-    font-family: 'Rubik','Lucida Sans';
+    font-family: 'Rubik', 'Lucida Sans';
     text-decoration: none;
     color: #1a1a1a;
     font-weight: 400;
--- frontend/brandly/src/main.js
+++ frontend/brandly/src/main.js
@@ -1,18 +1,15 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import App from './App.vue'
-import HomePage from './components/HomePage.vue'
-import LoginPage from './components/LoginPage.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import SignupPage from './pages/SignupPage.vue';
 import 'mdb-vue-ui-kit/css/mdb.min.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-  { path: '/', component: HomePage },
-  { path: '/login', component: LoginPage },
-  // { path: '/signup', component: SignupPage },
+  { path: '/', component: SignupPage },
 ]
 })
 
